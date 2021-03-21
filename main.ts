4878dc9779da6603225aee7a243c212ca8b7ef19
Dave Chan <davechan@users.noreply.github.com>
2021-03-22 00:29:36 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
 radio.setGroup(99)
 basic.forever(function () {
     radio.sendValue("x", input.acceleration(Dimension.X))
     radio.sendValue("y", input.acceleration(Dimension.Y))
     radio.sendValue("z", input.acceleration(Dimension.Z))
+    radio.sendValue("d", input.compassHeading())
 })
